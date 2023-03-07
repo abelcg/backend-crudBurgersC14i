3ec38dbe01ed19f46483be58c2077b5e858590e7
abelcg <abelcg86@gmail.com>
2023-03-06 23:35:30 -0300

    change url connection

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const url = 'mongodb://127.0.0.1:27017/crudBurgersC14i' //localhost = 127.0.0.1
+// const url = 'mongodb://127.0.0.1:27017/crudBurgersC14i' //localhost = 127.0.0.1
+const url = 'mongodb+srv://crudBurgerC14i:4aJTVuvlOORAYXrK@cluster0.qrmjuxh.mongodb.net/crudBurgersC14i' 
 
 mongoose.connect(url);
 
